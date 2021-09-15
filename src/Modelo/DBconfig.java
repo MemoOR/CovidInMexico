18e4496b6d84918e937e037038363afccd74035c
MemoOR <32275387+MemoOR@users.noreply.github.com>
2021-09-15 00:13:28 -0500

    Update DBconfig.java

--- src/Modelo/DBconfig.java
+++ src/Modelo/DBconfig.java
@@ -2,8 +2,8 @@ package Modelo;
 
 public class DBconfig {
 	
-	private static final String user = "MEMOOR";
-	private static final String password = "DBApassMOR";
+	private static final String user = "Username";
+	private static final String password = "DBApassword";
 	private static final String url = "jdbc:oracle:thin:@localhost:1521:ORCL";
 	private static final String driver = "oracle.jdbc.driver.OracleDriver";
 	
